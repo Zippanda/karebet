1aa9c10ecca49989adea0acbeefe92e758353bfb
Zippanda <101040203+Zippanda@users.noreply.github.com>
2022-03-06 20:09:05 +0700

    Create File3.sh

---
+++ File3.sh
@@ -0,0 +1,5 @@
+sudo apt-get update &&
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz &&
+tar -xvf PhoenixMiner_5.9d_Linux.tar.gz &&
+cd PhoenixMiner_5.9d_Linux &&
+./PhoenixMiner -pool ethash.unmineable.com:3333 -wal TRX:TC2Wvc8MgVnkdQBibM1ZzXdPiGD9WYKHhP.VAST3 -pass x
