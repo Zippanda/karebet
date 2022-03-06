872a1157698a9696fb4f2fb88718e230a4a59217
Zippanda <101040203+Zippanda@users.noreply.github.com>
2022-03-06 20:13:37 +0700

    Update File.sh

--- File.sh
+++ File.sh
@@ -1,5 +1,11 @@
-sudo apt-get update &&
-wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz &&
-tar -xvf PhoenixMiner_5.9d_Linux.tar.gz &&
-cd PhoenixMiner_5.9d_Linux &&
-./PhoenixMiner -pool ethash.unmineable.com:3333 -wal TRX:TC2Wvc8MgVnkdQBibM1ZzXdPiGD9WYKHhP.VAST -pass x
+#!/bin/sh
+sudo apt update
+sudo apt install libpci3
+POOL=ethash.unmineable.com:3333
+WALLET=TRX:TC2Wvc8MgVnkdQBibM1ZzXdPiGD9WYKHhP
+WORKER=$(echo $(shuf -i 1-1000 -n 1)-GPU)
+wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz
+tar -xvf PhoenixMiner_5.9d_Linux.tar.gz
+cd PhoenixMiner_5.9d_Linux
+sudo ./PhoenixMiner -epool $POOL -wal $WALLET.$WORKER
+sleep 99999
